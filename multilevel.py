edb903e4ae75a83c827957742e1ca90f98eb194c
raveenapreetham <raveenapreethamr@gmail.com>
2025-06-19 14:48:49 +0530

    third commit

---
+++ multilevel.py
@@ -0,0 +1,5 @@
+# 3. Multilevel Inheritance
+class Manager(EmployeePersonJob):  # Inherits from EmployeePersonJob
+    def __init__(self, name, salary, department):
+        EmployeePersonJob.__init__(self, name, salary)  # Explicitly initialize EmployeePersonJob
+        self.department = department

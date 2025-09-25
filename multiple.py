6d6b6bdb581101df3eda4e916db324708d84da1e
Raveenapreetham <raveenapreethamr@gmail.com>
2025-09-25 22:38:36 +0530

    Delete multiple.py

--- multiple.py
+++
@@ -1,9 +0,0 @@
-# 2. Multiple Inheritance
-class Job:
-    def __init__(self, salary):
-        self.salary = salary
-
-class EmployeePersonJob(Employee, Job):  # Inherits from both Employee and Job
-    def __init__(self, name, salary):
-        Employee.__init__(self, name, salary)  # Initialize Employee
-        Job.__init__(self, salary)            # Initialize Job

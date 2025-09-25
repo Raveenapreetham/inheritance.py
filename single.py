eb197daf6b7d2b275d311277307e0123ac0ec125
Raveenapreetham <raveenapreethamr@gmail.com>
2025-09-25 22:38:20 +0530

    Delete single.py

--- single.py
+++
@@ -1,9 +0,0 @@
-# 1. Single Inheritance
-class Person:
-    def __init__(self, name):
-        self.name = name
-
-class Employee(Person):  # Employee inherits from Person
-    def __init__(self, name, salary):
-        super().__init__(name)
-        self.salary = salary

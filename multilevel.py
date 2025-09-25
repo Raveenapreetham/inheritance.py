53a53901f7834630098248bbc7893bec00e0c3ca
Raveenapreetham <raveenapreethamr@gmail.com>
2025-09-25 22:38:48 +0530

    Delete multilevel.py

--- multilevel.py
+++
@@ -1,5 +0,0 @@
-# 3. Multilevel Inheritance
-class Manager(EmployeePersonJob):  # Inherits from EmployeePersonJob
-    def __init__(self, name, salary, department):
-        EmployeePersonJob.__init__(self, name, salary)  # Explicitly initialize EmployeePersonJob
-        self.department = department
